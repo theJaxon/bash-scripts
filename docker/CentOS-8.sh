1b9f13aea902ae8059719aecff155529dd968141
Jaxon <mohamedmostels@gmail.com>
2020-08-17 21:13:51 +0200

    Update CentOS-8.sh

--- docker/CentOS-8.sh
+++ docker/CentOS-8.sh
@@ -1,11 +1,11 @@
-sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 
-sudo yum install -y yum-utils
+yum install -y yum-utils
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install -y docker-ce --nobest
+yum install -y docker-ce --nobest
 
-sudo systemctl enable --now docker
+systemctl enable --now docker
 
-sudo groupadd docker && sudo usermod -aG docker $USER
+groupadd docker && sudo usermod -aG docker $USER
