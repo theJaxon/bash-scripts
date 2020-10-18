8468d7c6bbacbcad7c839078d6a291e8d082bf08
Jaxon <mohamedmostels@gmail.com>
2020-10-18 13:38:06 +0200

    Create ansible_family.sh

---
+++ ansible/ansible_family.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo -n "Enter role name: "
+read role_name
+
+# Create parent directory
+mkdir -v "${role_name}_family" && cd $_
+
+# Create tasks and vars directory 
+mkdir -v vars tasks 
+
+supported_os=(Archlinux Debian RedHat Windows)
+for os in "${supported_os[@]}"
+do 
+task_name="${role_name}-${os}"
+var_name="$os"
+touch "tasks/${task_name}" "vars/${var_name}"
+done
